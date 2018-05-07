784beea0d9ff633e9865f6c7a2eb904dda5fc1ca
chenqinlian <chenqinlian27@gmail.com>
2018-05-07 09:25:27 -0400

    rewrite exec()

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -113,8 +113,19 @@ syscall_handler (struct intr_frame *f)
 
   case SYS_EXEC: // 2
     {
-      void* cmdline;
-      memread_user(f->esp + 4, &cmdline, sizeof(cmdline));
+      void* cmdline = *(char **)(f->esp+4);
+
+
+
+      //check buffer
+      check_buffer(f->esp+4, sizeof(cmdline));
+
+      //check cmdline
+      if( get_user((const uint8_t *)cmdline)<0){
+        sys_badmemory_access();
+      }
+
+      //printf("cmdline:%s\n", (const char*) cmdline);
 
       int return_code = sys_exec((const char*) cmdline);
       f->eax = (uint32_t) return_code;
